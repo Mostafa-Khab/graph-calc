a7faf394f4af73f50ba4e854d79c6a6bde05c89a
Most Khal <mostafa6khaled@gmail.com>
2024-06-20 20:09:39 +0300

    nothing important again

--- src/plug.cpp
+++ src/plug.cpp
@@ -9,10 +9,10 @@
 // we have a color for each function. if you want to change the color of a function.
 // define a color as in this example. use your function name appended by _color suffix.
 /*
- * for example, if your function is call func:
+ * for example, if your function is called func:
  * gfx::rgb func_color = gfx::gruv::blue;
  * 
- * for example, if your function is call X:
+ * for example, if your function is called X:
  * gfx::rgb X_color = gfx::gruv::blue;
  * 
  * NOTE: this is a must in our case. or the app will crash!!!
@@ -26,12 +26,12 @@ extern "C"
 
   float func(float x)
   {
-    return std::sin( -1.5 + x * 4.f) / 2.f * 1.f / 0.8 + 0.5;
+    return std::cos( -1.5 + x * 4.f) / 2.f * 1.f / 0.8 + 0.5;
   }
 
   float func2(float x)
   {
-    return std::cos( -1.5 + x * 4.f) / 2.f * 1.f / 0.8 + 0.5;
+    return std::sin( -1.5 + x * 4.f) / 2.f * 1.f / 0.8 + 0.5;
   }
 
   float start_index = -5.0;
